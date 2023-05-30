58130b0c1c25895641bda2f5714d7b64823c6b22
jungizz <songjy0118@gmail.com>
2023-05-30 14:11:17 +0900

    Make Room scene
    - Interaction with objects
    - Open chest with key
    - Ladder

---
+++ Assets/Scripts/OpenChest.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenChest : MonoBehaviour
+{
+    private Animator chestAnim;
+
+    private void Start()
+    {
+        chestAnim = GetComponent<Animator>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Key"))
+        {
+            chestAnim.SetBool("isOpen", true);
+            Invoke("CloseChest", 5f);
+        }
+    }
+
+    void CloseChest()
+    {
+        chestAnim.SetBool("isOpen", false);
+    }
+}
