34b39d6126d7f0e0a252baa079436ac1bcc315dc
배 현아 <gusdk3312@ajou.ac.kr>
2023-06-01 10:10:58 +0900

    add : Timer

--- Assets/Scripts/ItemList.cs
+++ Assets/Scripts/ItemList.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ItemList : MonoBehaviour
 {
@@ -9,6 +10,43 @@ public class ItemList : MonoBehaviour
     public GameObject check3;
     public GameObject check4;
 
+    public Text[] timeText;
+    float time = 120;
+    int min, sec;
+
+    void Start()
+    {
+        timeText[0].text = "02";
+        timeText[1].text = "00";
+    }
+
+    void Update()
+    {
+        time -= Time.deltaTime;
+
+        min = (int)time / 60;
+        sec = ((int)time - min * 60) % 60;
+
+        if(min <= 0 && sec <= 0)
+        {
+            timeText[0].text = 0.ToString();
+            timeText[1].text = 0.ToString();
+            Time.timeScale = 0;
+        }
+        else
+        {
+            if(sec >= 60)
+            {
+                min += 1;
+                sec -= 60;
+            }
+            else
+            {
+                timeText[0].text = min.ToString();
+                timeText[1].text = sec.ToString();
+            }
+        }
+    }
 
     void OnCollisionEnter(Collision collision)
     {
