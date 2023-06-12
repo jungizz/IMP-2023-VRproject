3e9c9a8f3472116e8695616b7a6b6b2f66336142
msjh08 <msjh08@naver.com>
2023-06-12 22:20:43 +0900

    modify ending ui

--- Assets/Scripts/ItemList.cs
+++ Assets/Scripts/ItemList.cs
@@ -14,13 +14,28 @@ public class ItemList : MonoBehaviour
     public GameObject check7;
 
     public Text[] timeText;
-    float time = 120;
+    float time = 5;
     int min, sec;
+    int itemCount = 0;
+
+    public GameObject FailText;
+    public GameObject SuccessText;
+    public GameObject OverText;
+    public GameObject ClearText;
+    public GameObject BackButton;
+    public bool successCheck = false;
+    public bool failCheck = false;
+
+    public GameObject timerText1;
+    public GameObject timerText2;
+    public GameObject timerText3;
+    public GameObject itemPanel;
 
     void Start()
     {
         timeText[0].text = "02";
         timeText[1].text = "00";
+        itemCount = 0;
     }
 
     void Update()
@@ -30,11 +45,44 @@ public class ItemList : MonoBehaviour
         min = (int)time / 60;
         sec = ((int)time - min * 60) % 60;
 
+        if(itemCount >= 7)
+        {
+            timerText1.SetActive(false);
+            timerText2.SetActive(false);
+            timerText3.SetActive(false);
+            itemPanel.SetActive(false);
+
+            //SuccessText.SetActive(true); //show the result text
+            //Destroy(SuccessText, 4f);    //remove the result text after 4 seconds
+            StartCoroutine(Success());
+            Invoke("SuccessEnding", 4.5f);   //show the ending text after 4.5 seconds
+
+            successCheck = true;    //not using yet
+        }
+
+        //Timer
         if(min <= 0 && sec <= 0)
         {
             timeText[0].text = 0.ToString();
             timeText[1].text = 0.ToString();
-            Time.timeScale = 0;
+            //Time.timeScale = 0;
+
+            failCheck = true;
+
+            if (itemCount < 7 && failCheck == true)
+            {
+                timerText1.SetActive(false);
+                timerText2.SetActive(false);
+                timerText3.SetActive(false);
+                itemPanel.SetActive(false);
+
+                //FailText.SetActive(true); //show the result text
+                //Destroy(FailText, 4f);    //remove the result text after 4 seconds
+                StartCoroutine(Fail());
+                Invoke("FailEnding", 4.5f);   //show the ending text after 4.5 seconds
+
+                failCheck = false;
+            }
         }
         else
         {
@@ -58,37 +106,70 @@ public class ItemList : MonoBehaviour
             if(collision.gameObject == GameObject.Find("doll1"))
             {
                 check1.SetActive(true);
+                itemCount++;
             }
 
             if (collision.gameObject == GameObject.Find("doll2"))
             {
                 check2.SetActive(true);
+                itemCount++;
             }
 
             if (collision.gameObject == GameObject.Find("sword"))
             {
                 check3.SetActive(true);
+                itemCount++;
             }
 
             if (collision.gameObject == GameObject.Find("plant"))
             {
                 check4.SetActive(true);
+                itemCount++;
             }
 
             if (collision.gameObject == GameObject.Find("doll3"))
             {
                 check5.SetActive(true);
+                itemCount++;
             }
 
             if (collision.gameObject == GameObject.Find("lighter"))
             {
                 check6.SetActive(true);
+                itemCount++;
             }
 
             if (collision.gameObject == GameObject.Find("smartphone"))
             {
                 check7.SetActive(true);
+                itemCount++;
             }
         }
+    }
+
+    void SuccessEnding()
+    {
+        ClearText.SetActive(true);
+        BackButton.SetActive(true);
+    }
+
+    void FailEnding()
+    {
+        OverText.SetActive(true);
+        BackButton.SetActive(true);
+    }
+
+    IEnumerator Success()
+    {
+        SuccessText.SetActive(true); //show the result text
+        yield return new WaitForSeconds(4.0f);  //disable the result text after 4 seconds
+        SuccessText.SetActive(false);
+    }
+
+    IEnumerator Fail()
+    {
+        FailText.SetActive(true); //show the result text
+        yield return new WaitForSeconds(4.0f);  //disable the result text after 4 seconds
+        FailText.SetActive(false);
     }
 }
